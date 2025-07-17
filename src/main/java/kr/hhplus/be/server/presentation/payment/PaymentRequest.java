a8d2fe848dc2ca207fb76ccd4f58e107db607c57
mansol <qkr107781@gmail.com>
2025-07-18 05:48:45 +0900

    [feat]Payment Swagger 적용

--- src/main/java/kr/hhplus/be/server/presentation/payment/PaymentRequest.java
+++ src/main/java/kr/hhplus/be/server/presentation/payment/PaymentRequest.java
@@ -1,8 +1,12 @@
 package kr.hhplus.be.server.presentation.payment;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class PaymentRequest {
     public record Create(
+        @Schema(description = "사용자 ID", requiredMode = Schema.RequiredMode.REQUIRED)
         long user_id,
+        @Schema(description = "주문 ID", requiredMode = Schema.RequiredMode.REQUIRED)
         long order_id
     ){
     }
