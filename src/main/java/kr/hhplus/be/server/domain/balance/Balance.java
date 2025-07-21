bef4485ff75e3b58a1d306552ec2d88fd01dbcc9
mansollee <qkr107781@gmail.com>
2025-07-21 22:58:48 +0900

    [test,feat]Balance 도메인 단위테스트 및 기능 구현

--- src/main/java/kr/hhplus/be/server/domain/balance/Balance.java
+++ src/main/java/kr/hhplus/be/server/domain/balance/Balance.java
@@ -1,34 +1,14 @@
 package kr.hhplus.be.server.domain.balance;
 
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "balance")
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Balance {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "balance_id")
     private Long balanceId;
-
-    @Column(name = "user_id", nullable = false)
     private Long userId;
-
-    @Column(name = "balance", nullable = false)
     private Long balance;
-
-    @Column(name = "last_charge_date", nullable = false)
     private LocalDateTime lastChargeDate;
 
-    @Builder
     public Balance(Long balanceId, Long userId, Long balance, LocalDateTime lastChargeDate) {
         this.balanceId = balanceId;
         this.userId = userId;
@@ -36,20 +16,29 @@ public class Balance {
         this.lastChargeDate = lastChargeDate;
     }
 
+    public long getBalance(){
+        return balance;
+    }
+
     private final long MIN_CHARGE_AMOUNT = 1L;
     private final long NAX_CHARGE_AMOUNT_PER = 100_000L;
     private final long OVER_BALANCE = 1_000_000L;
 
-    public boolean validateChargeAmount(long chargeAmount, long balance){
+    public void validateChargeAmount(long chargeAmount, long balance){
         if(MIN_CHARGE_AMOUNT > chargeAmount){
-            return false;
+            throw new IllegalArgumentException("min charge illegal");
         }
         if(NAX_CHARGE_AMOUNT_PER < chargeAmount){
-            return false;
+            throw new IllegalArgumentException("max charge illegal");
         }
-        if(OVER_BALANCE < (chargeAmount + balance)){
-            return false;
+        if(OVER_BALANCE < (chargeAmount + balance)) {
+            throw new IllegalArgumentException("over charge illegal");
         }
-        return true;
+    }
+
+    public void charge(long chargeAmount){
+        validateChargeAmount(chargeAmount, balance);
+        this.balance += chargeAmount;
+        this.lastChargeDate = LocalDateTime.now();
     }
 }
--- src/test/java/kr/hhplus/be/server/domain/balance/BalanceTest.java
+++ src/test/java/kr/hhplus/be/server/domain/balance/BalanceTest.java
@@ -18,46 +18,56 @@ class BalanceTest {
 
     @BeforeEach
     void setUp() {
-        balance = Balance.builder()
-                    .user_id(1L)
-                    .balance(990_000L)
-                    .last_charge_date(LocalDateTime.now())
-                    .build();
+        balance = new Balance(1L,1L,990_000L,LocalDateTime.now());
     }
 
     @ParameterizedTest
-    @ValueSource(longs = {0L,100_001L})
+    @ValueSource(longs = {1L,500L,9_999L,1_000L,})
+    @DisplayName("[잔액 충전]충전 성공(잔액: 990_000L원, 충전: [1원, 500원, 9,999원, 1,000원]")
+    void charge(long chargeAmount){
+        //Given
+        Balance localBalance = new Balance(1L,1L,0L,LocalDateTime.now());
+        //When
+        localBalance.charge(chargeAmount);
+        //Then
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {-100L,0L})
     @DisplayName("[잔액 충전][최소 충전 금액 미달]입력받은 금액이 0 이하 일때 충전 실패(-100원,0원)")
-    void minUseChargeAmount(long charge_amount) {
+    void minUseChargeAmount(long chargeAmount) {
         //Given
         //@BeforeEach에서 진행
         //When
-        boolean result = balance.validateChargeAmount(charge_amount,balance.getBalance());
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
+                                            () -> balance.validateChargeAmount(chargeAmount, balance.getBalance()),"min charge illegal");
         //Then
-        assertFalse(result);
+        assertTrue(thrown.getMessage().contains("min charge illegal"));
     }
 
     @ParameterizedTest
     @ValueSource(longs = {100_001L,110_000L})
     @DisplayName("[잔액 충전][1회 최대 충전 금액 초과]입력받은 금액이 100,000 초과 일때 충전 실패(100,001원, 110,000원)")
-    void overChargeAmountPerCharge(long charge_amount) {
+    void overChargeAmountPerCharge(long chargeAmount) {
         //Given
         //@BeforeEach에서 진행
         //When
-        boolean result = balance.validateChargeAmount(charge_amount,balance.getBalance());
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
+                                            () -> balance.validateChargeAmount(chargeAmount, balance.getBalance()),"max charge illegal");
         //Then
-        assertFalse(result);
+        assertTrue(thrown.getMessage().contains("max charge illegal"));
     }
 
     @ParameterizedTest
     @ValueSource(longs = {10_001L,11_000L})
     @DisplayName("[잔액 충전][최대 잔고 초과 충전 요청]최대 잔고 초과 되도록 충전 요청 시 해당 요청 충전 실패(잔액: 990,000원, 충전: [10,001원,11,000원])")
-    void overMaxBalance(long charge_amount) {
+    void overMaxBalance(long chargeAmount) {
         //Given
         //@BeforeEach에서 진행
         //When
-        boolean result = balance.validateChargeAmount(charge_amount,balance.getBalance());
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
+                                            () -> balance.validateChargeAmount(chargeAmount, balance.getBalance()),"over charge illegal");
         //Then
-        assertFalse(result);
+        assertTrue(thrown.getMessage().contains("over charge illegal"));
     }
 }
