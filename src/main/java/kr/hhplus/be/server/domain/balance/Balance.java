2d5a6df5408fa1e26578016d0be63e3edcf8e11a
mansollee <qkr107781@gmail.com>
2025-07-22 00:44:21 +0900

    [style]Balance(잔액) 도메인 메소드에 기능 설명 주석 작성

--- src/main/java/kr/hhplus/be/server/domain/balance/Balance.java
+++ src/main/java/kr/hhplus/be/server/domain/balance/Balance.java
@@ -24,6 +24,11 @@ public class Balance {
     private final long NAX_CHARGE_AMOUNT_PER = 100_000L;
     private final long OVER_BALANCE = 1_000_000L;
 
+    /**
+     * 충전 금액 유효성 검증
+     * @param chargeAmount:충전 금액
+     * @param balance: 현재 잔액
+     */
     public void validateChargeAmount(long chargeAmount, long balance){
         if(MIN_CHARGE_AMOUNT > chargeAmount){
             throw new IllegalArgumentException("min charge illegal");
@@ -36,12 +41,21 @@ public class Balance {
         }
     }
 
+    /**
+     * 잔액 충전: 잔액 충전 전 충천 금액 유효성 검증 진행 후 기존 잔액에 충전 금액 더하여 저장
+     * @param chargeAmount:충전 금액
+     */
     public void charge(long chargeAmount){
         validateChargeAmount(chargeAmount, this.balance);
         this.balance += chargeAmount;
         this.lastChargeDate = LocalDateTime.now();
     }
 
+    /**
+     * 사용자 잔액 조회
+     * @param userId:사용자ID
+     * @return 현재 잔액
+     */
     public long selectBalance(long userId){
         if(this.userId != userId){
             throw new IllegalArgumentException("user compare fail");
