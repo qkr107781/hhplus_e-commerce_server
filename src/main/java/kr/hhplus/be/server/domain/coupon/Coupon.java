56e7b63e5fbe3d3475f73a1313dd0d4955e5ea92
mansol <qkr107781@gmail.com>
2025-07-22 15:27:55 +0900

    [test,feat]Coupon(쿠폰) 도메인 테스트코드 및 프로덕션코드 작성(쿠폰 발급)

--- src/main/java/kr/hhplus/be/server/domain/coupon/Coupon.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/Coupon.java
@@ -1,57 +1,22 @@
 package kr.hhplus.be.server.domain.coupon;
 
-import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name="coupon")
-@Getter
-@NoArgsConstructor
 public class Coupon {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "coupon_id")
     private Long couponId;
-
-    @Column(name = "coupon_name", length = 50, nullable = false)
     private String couponName;
-
-    @Column(name = "discount_price", nullable = false)
     private Long discountPrice;
-
-    @Column(name = "total_coupon_amount", nullable = false)
     private Long totalCouponAmount;
-
-    @Column(name = "remaining_coupon_amount", nullable = false)
     private Long remainingCouponAmount;
-
-    @Column(name = "min_use_price", nullable = false)
     private Long minUsePrice;
-
-    @Column(name = "issuance_start_time", nullable = false)
     private LocalDateTime issuanceStartTime;
-
-    @Column(name = "issuance_end_time", nullable = false)
     private LocalDateTime issuanceEndTime;
-
-    @Column(name = "use_limit_time", nullable = false)
     private Long useLimitTime;
-
-    @Column(name = "coupon_status", length = 10, nullable = false)
     private String couponStatus;
-
-    @Column(name = "reg_date", nullable = false)
     private LocalDateTime regDate;
 
-    @Builder
-    public Coupon(Long couponId, String couponName, Long discountPrice, Long totalCouponAmount,
-                  Long remainingCouponAmount, Long minUsePrice, LocalDateTime issuanceStartTime,
-                  LocalDateTime issuanceEndTime, Long useLimitTime, String couponStatus, LocalDateTime regDate) {
+    public Coupon(Long couponId, String couponName, Long discountPrice, Long totalCouponAmount, Long remainingCouponAmount, Long minUsePrice, LocalDateTime issuanceStartTime, LocalDateTime issuanceEndTime, Long useLimitTime, String couponStatus, LocalDateTime regDate) {
         this.couponId = couponId;
         this.couponName = couponName;
         this.discountPrice = discountPrice;
@@ -65,17 +30,71 @@ public class Coupon {
         this.regDate = regDate;
     }
 
-    public boolean validateCouponIssuance(Coupon coupon){
-        if(coupon.getRemainingCouponAmount() == 0){
-            System.out.println("잔여 수량 부족");
-            return false;
+    /**
+     * 쿠폰 발급 유효성 검증
+     * @throws Exception
+     */
+    public void validateCouponIssuance() throws Exception {
+        if(this.remainingCouponAmount == 0){
+            throw new Exception("empty remaining coupon");
         }
         LocalDateTime now = LocalDateTime.now();
-        if(coupon.getIssuanceStartTime().isAfter(now) ||
-                coupon.getIssuanceEndTime().isBefore(now)){
-            System.out.println("발급 시간 아님");
-            return false;
+        if(this.issuanceStartTime.isAfter(now) || this.issuanceEndTime.isBefore(now)){
+            throw new Exception("not issuing time");
         }
-        return true;
+        if(!this.couponStatus.equals("issuing")){
+            throw new Exception("not issuing status");
+        }
+    }
+
+    /**
+     * 쿠폰 갯수 차감
+     */
+    public void decreaseCoupon(){
+        this.remainingCouponAmount = this.remainingCouponAmount - 1L;
+    }
+
+    public Long getCouponId() {
+        return couponId;
+    }
+
+    public String getCouponName() {
+        return couponName;
+    }
+
+    public Long getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public Long getTotalCouponAmount() {
+        return totalCouponAmount;
+    }
+
+    public Long getRemainingCouponAmount() {
+        return remainingCouponAmount;
+    }
+
+    public Long getMinUsePrice() {
+        return minUsePrice;
+    }
+
+    public LocalDateTime getIssuanceStartTime() {
+        return issuanceStartTime;
+    }
+
+    public LocalDateTime getIssuanceEndTime() {
+        return issuanceEndTime;
+    }
+
+    public Long getUseLimitTime() {
+        return useLimitTime;
+    }
+
+    public String getCouponStatus() {
+        return couponStatus;
+    }
+
+    public LocalDateTime getRegDate() {
+        return regDate;
     }
 }
--- src/main/java/kr/hhplus/be/server/domain/coupon/CouponIssuedInfo.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/CouponIssuedInfo.java
@@ -1,40 +1,16 @@
 package kr.hhplus.be.server.domain.coupon;
 
-import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "coupon_issued_info")
-@Getter
-@NoArgsConstructor
 public class CouponIssuedInfo {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "coupon_issued_id")
     private Long couponIssuedId;
-
-    @Column(name = "user_id", nullable = false)
     private Long userId;
-
-    @Column(name = "use_yn", length = 1, nullable = false)
     private String useYn;
-
-    @Column(name = "issued_at", nullable = false)
     private LocalDateTime issuedAt;
-
-    @Column(name = "end_date", nullable = false)
     private LocalDateTime endDate;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="coupon_id", nullable = false)
     private Coupon coupon;
 
-    @Builder
     public CouponIssuedInfo(Long couponIssuedId, Long userId, String useYn, LocalDateTime issuedAt, LocalDateTime endDate, Coupon coupon) {
         this.couponIssuedId = couponIssuedId;
         this.userId = userId;
@@ -43,4 +19,29 @@ public class CouponIssuedInfo {
         this.endDate = endDate;
         this.coupon = coupon;
     }
+
+    public Long getCouponIssuedId() {
+        return couponIssuedId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUseYn() {
+        return useYn;
+    }
+
+    public LocalDateTime getIssuedAt() {
+        return issuedAt;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public Coupon getCoupon() {
+        return coupon;
+    }
+
 }
