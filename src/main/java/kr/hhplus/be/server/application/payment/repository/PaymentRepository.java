a533177867b4c3156a380dcb58f9d2ccb52602a3
mansollee <qkr107781@gmail.com>
2025-07-25 02:47:41 +0900

    [feat]Payment Persistence Layer 객체 생성

--- src/main/java/kr/hhplus/be/server/application/payment/repository/PaymentRepository.java
+++ src/main/java/kr/hhplus/be/server/application/payment/repository/PaymentRepository.java
@@ -1,9 +1,7 @@
 package kr.hhplus.be.server.application.payment.repository;
 
 import kr.hhplus.be.server.domain.payment.Payment;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PaymentRepository {
 
     Payment save(Payment payment);
