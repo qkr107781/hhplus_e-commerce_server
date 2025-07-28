3f8330a4a2bffe0e7589c826b2fc03b94247c8cb
mansollee <qkr107781@gmail.com>
2025-07-28 23:22:36 +0900

    [refactor] Balance 컨트롤러 usecase 호출

--- src/main/java/kr/hhplus/be/server/presentation/balance/BalanceController.java
+++ src/main/java/kr/hhplus/be/server/presentation/balance/BalanceController.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.presentation.balance;
 
 import kr.hhplus.be.server.application.balance.dto.BalanceRequest;
 import kr.hhplus.be.server.application.balance.dto.BalanceResponse;
+import kr.hhplus.be.server.application.balance.service.BalanceUseCase;
 import kr.hhplus.be.server.swagger.BalanceApiSpec;
 import kr.hhplus.be.server.util.DummyDataUtil;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +13,24 @@ import org.springframework.web.bind.annotation.*;
 public class BalanceController implements BalanceApiSpec {
 
     private final DummyDataUtil dummyDataUtil;
+    private final BalanceUseCase balanceUseCase;
 
-    public BalanceController(DummyDataUtil dummyDataUtil) {
+    public BalanceController(DummyDataUtil dummyDataUtil, BalanceUseCase balanceUseCase) {
         this.dummyDataUtil = dummyDataUtil;
+        this.balanceUseCase = balanceUseCase;
     }
 
     @PatchMapping("/user/balance/charge")
     @Override
     public ResponseEntity<BalanceResponse> charge(@RequestBody BalanceRequest request){
-        return ResponseEntity.ok(dummyDataUtil.getUserBalanceCharge());
+        //return ResponseEntity.ok(dummyDataUtil.getUserBalanceCharge());
+        return ResponseEntity.ok(balanceUseCase.charge(request));
     }
 
     @GetMapping("/user/balance/{userId}")
     @Override
     public ResponseEntity<BalanceResponse> selectBalanceByUserId(@PathVariable long userId){
-        return ResponseEntity.ok(dummyDataUtil.getUserSelectBalanceByUserId());
+        //return ResponseEntity.ok(dummyDataUtil.getUserSelectBalanceByUserId());
+        return ResponseEntity.ok(balanceUseCase.selectBalanceByUserId(userId));
     }
 }
