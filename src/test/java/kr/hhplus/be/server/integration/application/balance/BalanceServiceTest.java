187a0d9e3e2928420922bd9c748078b7a768b302
mansollee <qkr107781@gmail.com>
2025-08-01 05:28:40 +0900

    [test] Balance Application Layer 통합테스트

---
+++ src/test/java/kr/hhplus/be/server/integration/application/balance/BalanceServiceTest.java
@@ -0,0 +1,48 @@
+package kr.hhplus.be.server.integration.application.balance;
+
+import kr.hhplus.be.server.TestContainersConfiguration;
+import kr.hhplus.be.server.application.balance.dto.BalanceRequest;
+import kr.hhplus.be.server.application.balance.dto.BalanceResponse;
+import kr.hhplus.be.server.application.balance.repository.BalanceRepository;
+import kr.hhplus.be.server.application.balance.service.BalanceService;
+import kr.hhplus.be.server.domain.balance.Balance;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 테스트컨테이너에서 외부 DB 사용하도록 함
+@ActiveProfiles("test") //application-test.yml 읽어오도록 함
+@Sql(scripts = "/balance.sql",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS) //테스트 실행 시 해당 .sql 파일내의 쿼리 실행 -> 테이블 생성 후 실행됨
+@ContextConfiguration(classes = TestContainersConfiguration.class)//Spring boot Context 로딩 전 TestContainerConfiguration 읽어오게 하기 위함
+public class BalanceServiceTest {
+
+    @Autowired
+    BalanceRepository balanceRepository;
+
+    @Autowired
+    BalanceService balanceService;
+
+    @Test
+    @Transactional
+    @DisplayName("잔액 충전")
+    void charge(){
+        //Given
+        BalanceRequest balanceRequest = new BalanceRequest(1L,30_000L);
+
+        //When
+        BalanceResponse balanceResponse = balanceService.charge(balanceRequest);
+
+        //Then
+        assertEquals(30_000L,balanceResponse.balance());
+    }
+
+}
