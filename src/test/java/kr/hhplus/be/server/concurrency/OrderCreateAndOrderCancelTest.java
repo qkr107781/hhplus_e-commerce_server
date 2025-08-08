7f98108e92ad71fae7447158b43e4959ca41e117
mansollee <qkr107781@gmail.com>
2025-08-08 10:31:36 +0900

    [refactor] 주문 생성 vs 주문 취소 동시성 테스트 코드 및 테스트 데이터 쿼리 수정

--- src/test/java/kr/hhplus/be/server/concurrency/OrderCreateAndOrderCancelTest.java
+++ src/test/java/kr/hhplus/be/server/concurrency/OrderCreateAndOrderCancelTest.java
@@ -50,15 +50,15 @@ public class OrderCreateAndOrderCancelTest {
         //Given
         //파라미터 셋팅
         long userId_create = 1L; //주문 생성 요청 유저
-        //1: 1개, 2~4: 3개씩
-        List<Long> create_productOptionId = List.of(1L,2L,2L,2L,3L,3L,3L,4L,4L,4L);
+        //11: 1개, 12~14: 3개씩
+        List<Long> create_productOptionId = List.of(11L,12L,12L,12L,13L,13L,13L,14L,14L,14L);
         long couponId_create = 3L;
         OrderRequest.OrderCreate createOrder = new OrderRequest.OrderCreate(userId_create,create_productOptionId,couponId_create);
 
 
         long userId_cancel = 2L; //주문 취소 요청 유저
-        //2~4: 5개씩, 5: 1개
-        List<Long> cancel_productOptionId = List.of(2L,2L,2L,2L,2L,3L,3L,3L,3L,3L,4L,4L,4L,4L,4L,5L);
+        //12~14: 5개씩, 15: 1개
+        List<Long> cancel_productOptionId = List.of(12L,12L,12L,12L,12L,13L,13L,13L,13L,13L,14L,14L,14L,14L,14L,15L);
         long couponId_cancel = 3L;
         OrderRequest.OrderCancel cancelOrder = new OrderRequest.OrderCancel(userId_cancel,3L);
 
@@ -123,17 +123,17 @@ public class OrderCreateAndOrderCancelTest {
 
         //Then
         //재고 확인
-        ProductOption productOption_1 = productService.selectProductOptionByProductIdAndProductOptionId(1L,1L);
-        ProductOption productOption_2 = productService.selectProductOptionByProductIdAndProductOptionId(1L,2L);
-        ProductOption productOption_3 = productService.selectProductOptionByProductIdAndProductOptionId(1L,3L);
-        ProductOption productOption_4 = productService.selectProductOptionByProductIdAndProductOptionId(1L,4L);
-        ProductOption productOption_5 = productService.selectProductOptionByProductIdAndProductOptionId(1L,5L);
+        ProductOption productOption_11 = productService.selectProductOptionByProductIdAndProductOptionId(1L,11L);
+        ProductOption productOption_12 = productService.selectProductOptionByProductIdAndProductOptionId(1L,12L);
+        ProductOption productOption_13 = productService.selectProductOptionByProductIdAndProductOptionId(1L,13L);
+        ProductOption productOption_14 = productService.selectProductOptionByProductIdAndProductOptionId(1L,14L);
+        ProductOption productOption_15 = productService.selectProductOptionByProductIdAndProductOptionId(1L,15L);
 
-        System.out.println("옵션1 재고: "+productOption_1.getStockQuantity());
-        System.out.println("옵션2 재고: "+productOption_2.getStockQuantity());
-        System.out.println("옵션3 재고: "+productOption_3.getStockQuantity());
-        System.out.println("옵션4 재고: "+productOption_4.getStockQuantity());
-        System.out.println("옵션5 재고: "+productOption_5.getStockQuantity());
+        System.out.println("옵션1 재고: "+productOption_11.getStockQuantity());
+        System.out.println("옵션2 재고: "+productOption_12.getStockQuantity());
+        System.out.println("옵션3 재고: "+productOption_13.getStockQuantity());
+        System.out.println("옵션4 재고: "+productOption_14.getStockQuantity());
+        System.out.println("옵션5 재고: "+productOption_15.getStockQuantity());
 
         CouponIssuedInfo couponOrderCreate = couponService.selectCouponByCouponIdAndUserId(couponId_create,userId_create);
         CouponIssuedInfo couponOrderCancel = couponService.selectCouponByCouponIdAndUserId(couponId_cancel,userId_cancel);
@@ -141,11 +141,11 @@ public class OrderCreateAndOrderCancelTest {
         System.out.println("주문 생성 후 쿠폰 사용: "+couponOrderCreate.getUseYn());
         System.out.println("주문 취소 후 쿠폰 복구: "+couponOrderCancel.getUseYn());
 
-        assertEquals(29L,productOption_1.getStockQuantity());
-        assertEquals(32L,productOption_2.getStockQuantity());
-        assertEquals(32L,productOption_3.getStockQuantity());
-        assertEquals(32L,productOption_4.getStockQuantity());
-        assertEquals(31L,productOption_5.getStockQuantity());
+        assertEquals(29L,productOption_11.getStockQuantity());
+        assertEquals(32L,productOption_12.getStockQuantity());
+        assertEquals(32L,productOption_13.getStockQuantity());
+        assertEquals(32L,productOption_14.getStockQuantity());
+        assertEquals(31L,productOption_15.getStockQuantity());
         assertEquals("Y",couponOrderCreate.getUseYn());
         assertEquals("N",couponOrderCancel.getUseYn());
     }
@@ -287,9 +287,20 @@ public class OrderCreateAndOrderCancelTest {
         assertEquals(24L,productOption_3.getStockQuantity());
         assertEquals(24L,productOption_4.getStockQuantity());
         assertEquals(30L,productOption_5.getStockQuantity());
-        assertEquals("Y",couponOrderCreate_1.getUseYn());
-        assertEquals("Y",couponOrderCreate_2.getUseYn());
-        assertEquals("Y",couponOrderCreate_3.getUseYn());
+        if(couponEmptyErrorCount_1.get() > 0){
+            assertEquals("N",couponOrderCreate_1.getUseYn());
+            assertEquals("Y",couponOrderCreate_2.getUseYn());
+            assertEquals("Y",couponOrderCreate_3.getUseYn());
+        }else if(couponEmptyErrorCount_2.get() > 0){
+            assertEquals("Y",couponOrderCreate_1.getUseYn());
+            assertEquals("N",couponOrderCreate_2.getUseYn());
+            assertEquals("Y",couponOrderCreate_3.getUseYn());
+        }else if(couponEmptyErrorCount_3.get() > 0){
+            assertEquals("Y",couponOrderCreate_1.getUseYn());
+            assertEquals("Y",couponOrderCreate_2.getUseYn());
+            assertEquals("N",couponOrderCreate_3.getUseYn());
+        }
+
     }
 
 
@@ -298,24 +309,20 @@ public class OrderCreateAndOrderCancelTest {
     void orderCreateAndOrderCancel3() throws Exception {
         //Given
         //파라미터 셋팅
+        //6~8: 5개씩, 9: 1개
         long userId_cancel_1 = 2L; //주문 취소 요청 유저
-        //2~4: 5개씩, 5: 1개
         OrderRequest.OrderCancel cancelOrder_1 = new OrderRequest.OrderCancel(userId_cancel_1,4L);
 
         long userId_cancel_2 = 2L; //주문 취소 요청 유저
-        //2~4: 5개씩, 5: 1개
         OrderRequest.OrderCancel cancelOrder_2 = new OrderRequest.OrderCancel(userId_cancel_2,5L);
 
         long userId_cancel_3 = 2L; //주문 취소 요청 유저
-        //2~4: 5개씩, 5: 1개
         OrderRequest.OrderCancel cancelOrder_3 = new OrderRequest.OrderCancel(userId_cancel_3,6L);
 
         long userId_cancel_4 = 2L; //주문 취소 요청 유저
-        //2~4: 5개씩, 5: 1개
         OrderRequest.OrderCancel cancelOrder_4 = new OrderRequest.OrderCancel(userId_cancel_4,7L);
 
         long userId_cancel_5 = 2L; //주문 취소 요청 유저
-        //2~4: 5개씩, 5: 1개
         OrderRequest.OrderCancel cancelOrder_5 = new OrderRequest.OrderCancel(userId_cancel_5,8L);
 
         //동시성 테스트 준비
@@ -443,29 +450,27 @@ public class OrderCreateAndOrderCancelTest {
 
         //Then
         //재고 확인
-        ProductOption productOption_1 = productService.selectProductOptionByProductIdAndProductOptionId(1L,1L);
-        ProductOption productOption_2 = productService.selectProductOptionByProductIdAndProductOptionId(1L,2L);
-        ProductOption productOption_3 = productService.selectProductOptionByProductIdAndProductOptionId(1L,3L);
-        ProductOption productOption_4 = productService.selectProductOptionByProductIdAndProductOptionId(1L,4L);
-        ProductOption productOption_5 = productService.selectProductOptionByProductIdAndProductOptionId(1L,5L);
+        ProductOption productOption_6 = productService.selectProductOptionByProductIdAndProductOptionId(1L,6L);
+        ProductOption productOption_7 = productService.selectProductOptionByProductIdAndProductOptionId(1L,7L);
+        ProductOption productOption_8 = productService.selectProductOptionByProductIdAndProductOptionId(1L,8L);
+        ProductOption productOption_9 = productService.selectProductOptionByProductIdAndProductOptionId(1L,9L);
 
-        System.out.println("옵션1 재고: "+productOption_1.getStockQuantity());
-        System.out.println("옵션2 재고: "+productOption_2.getStockQuantity());
-        System.out.println("옵션3 재고: "+productOption_3.getStockQuantity());
-        System.out.println("옵션4 재고: "+productOption_4.getStockQuantity());
-        System.out.println("옵션5 재고: "+productOption_5.getStockQuantity());
+        System.out.println("옵션6 재고: "+productOption_6.getStockQuantity());
+        System.out.println("옵션7 재고: "+productOption_7.getStockQuantity());
+        System.out.println("옵션8 재고: "+productOption_8.getStockQuantity());
+        System.out.println("옵션9 재고: "+productOption_9.getStockQuantity());
 
         CouponIssuedInfo couponOrderCreate_1 = couponService.selectCouponByCouponIdAndUserId(4L,userId_cancel_1);
-        CouponIssuedInfo couponOrderCreate_2 = couponService.selectCouponByCouponIdAndUserId(5L,userId_cancel_1);
-        CouponIssuedInfo couponOrderCreate_3 = couponService.selectCouponByCouponIdAndUserId(6L,userId_cancel_1);
-        CouponIssuedInfo couponOrderCreate_4 = couponService.selectCouponByCouponIdAndUserId(7L,userId_cancel_1);
-        CouponIssuedInfo couponOrderCreate_5 = couponService.selectCouponByCouponIdAndUserId(8L,userId_cancel_1);
+        CouponIssuedInfo couponOrderCreate_2 = couponService.selectCouponByCouponIdAndUserId(5L,userId_cancel_2);
+        CouponIssuedInfo couponOrderCreate_3 = couponService.selectCouponByCouponIdAndUserId(6L,userId_cancel_3);
+        CouponIssuedInfo couponOrderCreate_4 = couponService.selectCouponByCouponIdAndUserId(7L,userId_cancel_4);
+        CouponIssuedInfo couponOrderCreate_5 = couponService.selectCouponByCouponIdAndUserId(8L,userId_cancel_5);
 
         System.out.println("주문 취소 후 쿠폰 사용_1: "+couponOrderCreate_1.getUseYn());
         System.out.println("주문 취소 후 쿠폰 사용_2: "+couponOrderCreate_2.getUseYn());
         System.out.println("주문 취소 후 쿠폰 사용_3: "+couponOrderCreate_3.getUseYn());
-        System.out.println("주문 취소 후 쿠폰 사용_3: "+couponOrderCreate_4.getUseYn());
-        System.out.println("주문 취소 후 쿠폰 사용_3: "+couponOrderCreate_5.getUseYn());
+        System.out.println("주문 취소 후 쿠폰 사용_4: "+couponOrderCreate_4.getUseYn());
+        System.out.println("주문 취소 후 쿠폰 사용_5: "+couponOrderCreate_5.getUseYn());
 
         //각 스레드 실패 건수 확인
         System.out.println("1번 쓰레드 실패 건수: "+couponEmptyErrorCount_1.get());
@@ -474,16 +479,40 @@ public class OrderCreateAndOrderCancelTest {
         System.out.println("4번 쓰레드 실패 건수: "+couponEmptyErrorCount_4.get());
         System.out.println("5번 쓰레드 실패 건수: "+couponEmptyErrorCount_5.get());
 
-        assertEquals(30L,productOption_1.getStockQuantity());
-        assertEquals(50L,productOption_2.getStockQuantity());
-        assertEquals(50L,productOption_3.getStockQuantity());
-        assertEquals(50L,productOption_4.getStockQuantity());
-        assertEquals(34L,productOption_5.getStockQuantity());
-        assertEquals("N",couponOrderCreate_1.getUseYn());
-        assertEquals("N",couponOrderCreate_2.getUseYn());
-        assertEquals("N",couponOrderCreate_3.getUseYn());
-        assertEquals("N",couponOrderCreate_4.getUseYn());
-        assertEquals("N",couponOrderCreate_5.getUseYn());
+        assertEquals(50L,productOption_6.getStockQuantity());
+        assertEquals(50L,productOption_7.getStockQuantity());
+        assertEquals(50L,productOption_8.getStockQuantity());
+        assertEquals(34L,productOption_9.getStockQuantity());
+        if(couponEmptyErrorCount_1.get() > 0){
+            assertEquals("Y",couponOrderCreate_1.getUseYn());
+            assertEquals("N",couponOrderCreate_2.getUseYn());
+            assertEquals("N",couponOrderCreate_3.getUseYn());
+            assertEquals("N",couponOrderCreate_4.getUseYn());
+            assertEquals("N",couponOrderCreate_5.getUseYn());
+        }else if(couponEmptyErrorCount_2.get() > 0){
+            assertEquals("N",couponOrderCreate_1.getUseYn());
+            assertEquals("Y",couponOrderCreate_2.getUseYn());
+            assertEquals("N",couponOrderCreate_3.getUseYn());
+            assertEquals("N",couponOrderCreate_4.getUseYn());
+            assertEquals("N",couponOrderCreate_5.getUseYn());
+        }else if(couponEmptyErrorCount_3.get() > 0){
+            assertEquals("N",couponOrderCreate_1.getUseYn());
+            assertEquals("N",couponOrderCreate_2.getUseYn());
+            assertEquals("Y",couponOrderCreate_3.getUseYn());
+            assertEquals("N",couponOrderCreate_4.getUseYn());
+            assertEquals("N",couponOrderCreate_5.getUseYn());
+        }else if(couponEmptyErrorCount_4.get() > 0){
+            assertEquals("N",couponOrderCreate_1.getUseYn());
+            assertEquals("N",couponOrderCreate_2.getUseYn());
+            assertEquals("N",couponOrderCreate_3.getUseYn());
+            assertEquals("Y",couponOrderCreate_4.getUseYn());
+            assertEquals("N",couponOrderCreate_5.getUseYn());
+        }else if(couponEmptyErrorCount_5.get() > 0){
+            assertEquals("N",couponOrderCreate_1.getUseYn());
+            assertEquals("N",couponOrderCreate_2.getUseYn());
+            assertEquals("N",couponOrderCreate_3.getUseYn());
+            assertEquals("N",couponOrderCreate_4.getUseYn());
+            assertEquals("Y",couponOrderCreate_5.getUseYn());
+        }
     }
-
 }
--- src/test/resources/concurrencyOrder.sql
+++ src/test/resources/concurrencyOrder.sql
@@ -17,6 +17,36 @@ VALUES(4,'Y', 30000, 1, now(), '옵션4', 30, 50);
 INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
 VALUES(5,'Y', 30000, 1, now(), '옵션5', 30, 50);
 
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(6,'Y', 10000, 1, now(), '옵션6', 30, 50);
+
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(7,'Y', 20000, 1, now(), '옵션7', 30, 50);
+
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(8,'Y', 30000, 1, now(), '옵션8', 30, 50);
+
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(9,'Y', 30000, 1, now(), '옵션9', 30, 50);
+
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(10,'Y', 30000, 1, now(), '옵션10', 30, 50);
+
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(11,'Y', 30000, 1, now(), '옵션10', 30, 50);
+
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(12,'Y', 30000, 1, now(), '옵션10', 30, 50);
+
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(13,'Y', 30000, 1, now(), '옵션10', 30, 50);
+
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(14,'Y', 30000, 1, now(), '옵션10', 30, 50);
+
+INSERT INTO product_option(product_option_id,sales_yn, price, product_id, reg_date, option_name, stock_quantity, total_quantity)
+VALUES(15,'Y', 30000, 1, now(), '옵션10', 30, 50);
+
 --사용자 소유 쿠폰 정보 입력
 INSERT INTO coupon
 (coupon_id, discount_price, issuance_end_time, issuance_start_time, reg_date, remaining_coupon_amount, total_coupon_amount, coupon_status, coupon_name)
@@ -74,95 +104,95 @@ INSERT INTO `order` (order_id,coupon_discount_price, coupon_id, order_date, tota
 VALUES(3, 1000, 3, now(), 50000, 2, 'pending_payment');
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(3, 1, 2, 20000, 5);
+VALUES(3, 1, 12, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(3, 1, 3, 20000, 5);
+VALUES(3, 1, 13, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(3, 1, 4, 20000, 5);
+VALUES(3, 1, 14, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(3, 1, 5, 20000, 1);
+VALUES(3, 1, 15, 20000, 1);
 
 
 
 --주문 취소 정보 입력
 INSERT INTO `order` (order_id,coupon_discount_price, coupon_id, order_date, total_price, user_id, order_status)
-VALUES(4, 1000, 3, now(), 50000, 2, 'pending_payment');
+VALUES(4, 1000, 4, now(), 50000, 2, 'pending_payment');
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(4, 1, 2, 20000, 5);
+VALUES(4, 1, 6, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(4, 1, 3, 20000, 5);
+VALUES(4, 1, 7, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(4, 1, 4, 20000, 5);
+VALUES(4, 1, 8, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(4, 1, 5, 20000, 1);
+VALUES(4, 1, 9, 20000, 1);
 
 --주문 취소 정보 입력
 INSERT INTO `order` (order_id,coupon_discount_price, coupon_id, order_date, total_price, user_id, order_status)
-VALUES(5, 1000, 3, now(), 50000, 2, 'pending_payment');
+VALUES(5, 1000, 5, now(), 50000, 2, 'pending_payment');
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(5, 1, 2, 20000, 5);
+VALUES(5, 1, 6, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(5, 1, 3, 20000, 5);
+VALUES(5, 1, 7, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(5, 1, 4, 20000, 5);
+VALUES(5, 1, 8, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(5, 1, 5, 20000, 1);
+VALUES(5, 1, 9, 20000, 1);
 
 --주문 취소 정보 입력
 INSERT INTO `order` (order_id,coupon_discount_price, coupon_id, order_date, total_price, user_id, order_status)
-VALUES(6, 1000, 3, now(), 50000, 2, 'pending_payment');
+VALUES(6, 1000, 6, now(), 50000, 2, 'pending_payment');
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(6, 1, 2, 20000, 5);
+VALUES(6, 1, 6, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(6, 1, 3, 20000, 5);
+VALUES(6, 1, 7, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(6, 1, 4, 20000, 5);
+VALUES(6, 1, 8, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(6, 1, 5, 20000, 1);
+VALUES(6, 1, 9, 20000, 1);
 
 --주문 취소 정보 입력
 INSERT INTO `order` (order_id,coupon_discount_price, coupon_id, order_date, total_price, user_id, order_status)
-VALUES(7, 1000, 3, now(), 50000, 2, 'pending_payment');
+VALUES(7, 1000, 7, now(), 50000, 2, 'pending_payment');
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(7, 1, 2, 20000, 5);
+VALUES(7, 1, 6, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(7, 1, 3, 20000, 5);
+VALUES(7, 1, 7, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(7, 1, 4, 20000, 5);
+VALUES(7, 1, 8, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(7, 1, 5, 20000, 1);
+VALUES(7, 1, 9, 20000, 1);
 
 --주문 취소 정보 입력
 INSERT INTO `order` (order_id,coupon_discount_price, coupon_id, order_date, total_price, user_id, order_status)
-VALUES(8, 1000, 3, now(), 50000, 2, 'pending_payment');
+VALUES(8, 1000, 8, now(), 50000, 2, 'pending_payment');
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(8, 1, 2, 20000, 5);
+VALUES(8, 1, 6, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(8, 1, 3, 20000, 5);
+VALUES(8, 1, 7, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(8, 1, 4, 20000, 5);
+VALUES(8, 1, 8, 20000, 5);
 
 INSERT INTO order_product(order_id, product_id, product_option_id, product_price, product_quantity)
-VALUES(8, 1, 5, 20000, 1);
+VALUES(8, 1, 9, 20000, 1);
