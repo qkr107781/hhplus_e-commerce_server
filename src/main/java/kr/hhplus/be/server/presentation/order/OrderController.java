c29fcd579146c5fbd31cfcbf9d02454aab2da4fc
mansollee <qkr107781@gmail.com>
2025-08-14 03:24:30 +0900

    [refactor] 피드백 반영 - 어플리케이션 레이어 미사용 메소드 사용하도록 수정

--- src/main/java/kr/hhplus/be/server/presentation/order/OrderController.java
+++ src/main/java/kr/hhplus/be/server/presentation/order/OrderController.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.presentation.order;
 
 import kr.hhplus.be.server.application.order.dto.OrderRequest;
 import kr.hhplus.be.server.application.order.dto.OrderResponse;
+import kr.hhplus.be.server.application.order.service.OrderUseCase;
 import kr.hhplus.be.server.swagger.OrderApiSpec;
 import kr.hhplus.be.server.util.DummyDataUtil;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +13,23 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController implements OrderApiSpec {
 
     private final DummyDataUtil dummyDataUtil;
+    private final OrderUseCase orderUseCase;
 
-    public OrderController(DummyDataUtil dummyDataUtil) {
+    public OrderController(DummyDataUtil dummyDataUtil, OrderUseCase orderUseCase) {
         this.dummyDataUtil = dummyDataUtil;
+        this.orderUseCase = orderUseCase;
     }
 
     @PostMapping("/order")
     @Override
-    public ResponseEntity<OrderResponse.OrderDTO> orderCreate(@RequestBody OrderRequest.OrderCreate request){
-        return ResponseEntity.ok(dummyDataUtil.getOrderCreate());
+    public ResponseEntity<OrderResponse.OrderDTO> orderCreate(@RequestBody OrderRequest.OrderCreate request) throws Exception {
+        return ResponseEntity.ok(orderUseCase.createOrder(request));
+    }
+
+    @PatchMapping("/order")
+    @Override
+    public ResponseEntity<OrderResponse.OrderDTO> orderCancel(@RequestBody OrderRequest.OrderCancel request) throws Exception {
+        return ResponseEntity.ok(orderUseCase.cancelOrder(request));
     }
 
 }
--- src/main/java/kr/hhplus/be/server/swagger/OrderApiSpec.java
+++ src/main/java/kr/hhplus/be/server/swagger/OrderApiSpec.java
@@ -9,6 +9,9 @@ import org.springframework.http.ResponseEntity;
 @Tag(name = "주문", description = "주문 관련 API")
 public interface OrderApiSpec {
 
-    @Operation(summary = "주문")
-    ResponseEntity<OrderResponse.OrderDTO> orderCreate( OrderRequest.OrderCreate request);
+    @Operation(summary = "주문 생성")
+    ResponseEntity<OrderResponse.OrderDTO> orderCreate( OrderRequest.OrderCreate request) throws Exception;
+
+    @Operation(summary = "주문 취소")
+    ResponseEntity<OrderResponse.OrderDTO> orderCancel( OrderRequest.OrderCancel request) throws Exception;
 }
