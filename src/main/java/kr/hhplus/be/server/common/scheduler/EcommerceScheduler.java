9e89c35c557f7b7728df70203926afb6defd696e
mansollee <qkr107781@gmail.com>
2025-08-15 07:28:35 +0900

    [feat] 인기 상품 조회 레디스 캐시 전략 적용

--- src/main/java/kr/hhplus/be/server/common/scheduler/EcommerceScheduler.java
+++ src/main/java/kr/hhplus/be/server/common/scheduler/EcommerceScheduler.java
@@ -1,50 +1,42 @@
-package kr.hhplus.be.server.application.product.facade;
+package kr.hhplus.be.server.common.scheduler;
 
 import kr.hhplus.be.server.application.order.dto.OrderProductSummary;
 import kr.hhplus.be.server.application.order.service.OrderProductService;
 import kr.hhplus.be.server.application.order.service.OrderService;
 import kr.hhplus.be.server.application.product.dto.ProductResponse;
 import kr.hhplus.be.server.application.product.service.ProductStatisticsService;
-import kr.hhplus.be.server.application.product.service.ProductStatisticsUseCase;
 import kr.hhplus.be.server.domain.order.Order;
 import kr.hhplus.be.server.domain.order.OrderProduct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class ProductFacadeService implements ProductStatisticsUseCase {
+@Component
+public class EcommerceScheduler {
 
     private final OrderService orderService;
     private final OrderProductService orderProductService;
     private final ProductStatisticsService productStatisticsService;
 
-    public ProductFacadeService(OrderService orderService, OrderProductService orderProductService, ProductStatisticsService productStatisticsService) {
+    public EcommerceScheduler(OrderService orderService, OrderProductService orderProductService, ProductStatisticsService productStatisticsService) {
         this.orderService = orderService;
         this.orderProductService = orderProductService;
         this.productStatisticsService = productStatisticsService;
     }
+
     private static final String CACHE_NAME = "topSalesProducts";
     private static final String CACHE_KEY = "'top5:sales:last3days'";
-    /**
-     * 오늘 기준 4일 전부터 1일 전까지의 데이터 중 salesQuantity 기준 상위 5개를 조회합니다.
-     * 예: 오늘이 7월 25일이면, 7월 21일 부터 7월 24일 까지의 데이터를 조회합니다.
-     * @return 상위 5개 통계 데이터 리스트
-     */
-    @Cacheable(
+
+    @CachePut(
             cacheNames = CACHE_NAME,
-            key = CACHE_KEY,
-            unless = "#result == null  || #result.isEmpty()"
+            key = CACHE_KEY
     )
-    @Transactional(readOnly = true)
-    @Override
-    public List<ProductResponse.Statistics> selectTop5SalesProductBySpecificRange() {
+    @Scheduled(cron = "0 0 0 * * *") // 매일 0:00 실행
+    public List<ProductResponse.Statistics> productStatisticsCache() {
         List<OrderProduct> orderProductListByBefore3Days = new ArrayList<>();
 
         LocalDate endDate = LocalDate.now();
@@ -62,5 +54,4 @@ public class ProductFacadeService implements ProductStatisticsUseCase {
 
         return productStatisticsService.selectTop5SalesProductBySpecificRange(top5OrderProductList);
     }
-
 }
