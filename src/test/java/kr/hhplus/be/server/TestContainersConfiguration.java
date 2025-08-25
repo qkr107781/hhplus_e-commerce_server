efce9214d28af852db4e12cfdc4928a31506af83
mansollee <qkr107781@gmail.com>
2025-08-25 13:15:32 +0900

    [refactor] 테스트컨테이너 mysql 한개만 뜨도록 수정

--- src/test/java/kr/hhplus/be/server/TestContainersConfiguration.java
+++ src/test/java/kr/hhplus/be/server/TestContainersConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
@@ -17,44 +16,40 @@ public class TestContainersConfiguration { // @TestConfiguration 어노테이션
 
 	//########################## MySQL TestContainer Configuration ##########################//
 
-	@Bean // Spring 빈으로 등록
-	@ServiceConnection // <<-- MySQLContainer를 DataSource에 자동으로 연결하도록 지시
-	public MySQLContainer<?> mySQLContainer() {
-		System.out.println("########## TestcontainersConfiguration: @ServiceConnection MySQL 컨테이너 빈 생성 시작 ##########");
-		MySQLContainer<?> container = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.33"))
-				.withDatabaseName("testdb")
-				.withUsername("test")
-				.withPassword("test");
-		// 컨테이너.start()는 @ServiceConnection이 알아서 해줍니다. 여기에 명시적으로 호출하지 마세요.
-		System.out.println("########## TestcontainersConfiguration: @ServiceConnection MySQL 컨테이너 빈 생성 완료 ##########");
-		return container;
+	@ServiceConnection
+	@Bean
+	public static MySQLContainer<?> mySQLContainer() {
+		return MySQLHolder.INSTANCE;
+	}
+
+	private static class MySQLHolder {
+		private static final MySQLContainer<?> INSTANCE =
+				new MySQLContainer<>(DockerImageName.parse("mysql:8.0.33"))
+						.withDatabaseName("testdb")
+						.withUsername("test")
+						.withPassword("test");
+		static { INSTANCE.start(); }
 	}
 
 	//########################## Redis TestContainer Configuration ##########################//
 
+
 	private static final int REDIS_PORT = 6379;
 
-	// Testcontainers로 Redis 컨테이너 정의. Redis 클러스터가 아닌 단일 서버를 사용
-	// Redis의 기본 포트인 6379를 노출
-	@Container
-	private static final GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.2.4-alpine"))
-			.withExposedPorts(6379);
+	private static final GenericContainer<?> redisContainer =
+			new GenericContainer<>(DockerImageName.parse("redis:7.2.4-alpine"))
+					.withExposedPorts(REDIS_PORT);
 
 	static {
-		// Testcontainers 컨테이너를 시작하고,
-		// 시스템 프로퍼티에 동적으로 할당된 Redis 서버 주소를 설정
-		redis.start();
-		String redisAddress = "redis://" + redis.getHost() + ":" + redis.getFirstMappedPort();
-		System.setProperty("redisson.address", redisAddress);
+		redisContainer.start();
 	}
 
-	// RedissonClient 빈을 직접 생성하고 동적 주소를 설정
 	@Bean
 	public RedissonClient redissonClient() {
 		Config config = new Config();
-		String redisAddress = System.getProperty("redisson.address");
 		config.setCodec(StringCodec.INSTANCE);
-		config.useSingleServer().setAddress(redisAddress);
+		config.useSingleServer()
+				.setAddress("redis://" + redisContainer.getHost() + ":" + redisContainer.getFirstMappedPort());
 		return Redisson.create(config);
 	}
 }
