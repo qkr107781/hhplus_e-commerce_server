ea765f0a15db08bbb3dd5fecd91d639ffe4103da
mansollee <qkr107781@gmail.com>
2025-09-05 06:57:22 +0900

    [feat] [Common] 선착순 쿠폰 발급 상수 추가

--- src/main/java/kr/hhplus/be/server/common/kafka/KafkaConstants.java
+++ src/main/java/kr/hhplus/be/server/common/kafka/KafkaConstants.java
@@ -5,7 +5,6 @@ public final class KafkaConstants {
     public static final String PAYMENT_COMPLETE_TOPIC = "payment-complete-topic";
     public static final String PAYMENT_COMPLETE_GROUP = "payment-complete-group";
 
-    public static final String COUPON_ISSUED_TOPIC = "coupon-issued-topic";
     public static final String COUPON_ISSUED_GROUP = "coupon-issue-group";
 
     // 인스턴스 생성을 막기 위함
