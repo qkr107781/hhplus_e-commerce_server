9d05510b2bc2dbb714a295d63d5f8f4106059400
mansollee <qkr107781@gmail.com>
2025-09-05 06:53:55 +0900

    [feat] [Persistence] 선착순 쿠폰 발급 Outbox 저장 메소드 추가, 쿠폰 잔여 갯수 원자적 감소 쿼리 추가

--- src/main/java/kr/hhplus/be/server/persistence/coupon/CouponJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/persistence/coupon/CouponJpaRepository.java
@@ -4,6 +4,9 @@ import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +20,9 @@ public interface CouponJpaRepository extends JpaRepository<Coupon,Long> {
     Coupon findById(long couponId);
 
     List<Coupon> findByCouponStatus(String status);
+
+    @Modifying
+    @Query("UPDATE Coupon c SET c.remainingCouponAmount = c.remainingCouponAmount - 1 " +
+            "WHERE c.couponId = :couponId AND c.remainingCouponAmount > 0")
+    int decreaseRemainingCoupon(@Param("couponId") Long couponId);
 }
